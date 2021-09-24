9db21006eb0fa65adee621cbb1cd3802b8a3ff79
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-09-24 04:15:27 -0500

    Separate function from script

--- practicas_calificadas/pc_01/scripts/ex_05_01.m
+++ practicas_calificadas/pc_01/scripts/ex_05_01.m
@@ -8,31 +8,15 @@ fprintf('Roots of f(x) = 2x^3-6x-1\n');
 
 fprintf('\n');
 g = @(x) (2 * x^3 - 1) / 6;
-root = fixed_point_iteration(g, 1, eps, max_steps);
+root = fixed_point_iteration(g, 1, eps, max_steps, -0.168254);
 fprintf('First root: %.6f\n', root);
 
 fprintf('\n');
 g = @(x) ((6 * x + 1) / 2)^(1/3);
-root = fixed_point_iteration(g, 2, eps, max_steps);
+root = fixed_point_iteration(g, 2, eps, max_steps, 1.810038);
 fprintf('Second root: %.6f\n', root);
 
 fprintf('\n');
 g = @(x) (4 * x^3 + 1) / (6 * x^2 - 6);
-root = fixed_point_iteration(g, -2, eps, max_steps);
+root = fixed_point_iteration(g, -2, eps, max_steps, -1.641784);
 fprintf('Third root: %.6f\n', root);
-
-% Returns an approximate solution to f(x) = x with a tolerance of eps
-% and at most max_steps steps (to prevent infinite loop)
-% Usage: root = fixed_point_iteration(f, guess, eps, max_steps);
-
-function x = fixed_point_iteration(f, guess, eps, max_steps)
-    % First step
-    x = f(guess);
-    steps = 1;
-    fprintf('step: %d x: %.6f\n', steps, x);
-    while abs(x - f(x)) > eps && steps < max_steps
-        x = f(x);
-        steps = steps + 1;
-        fprintf('step: %d x: %.6f\n', steps, x);
-    end
-end
